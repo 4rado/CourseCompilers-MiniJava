b912add8aa7ff2fef65eb29474623c5864183e95
Andrey <andrei.ignatev@phystech.edu>
2015-10-20 14:52:01 +0300

    Дописаны все Expressions

--- CourseCompilers-MiniJava/CTypes.h
+++ CourseCompilers-MiniJava/CTypes.h
@@ -130,18 +130,142 @@ public:
 	};
 };
 
-class CExpBinary : public CExp
-{
+
+class CExpInSquareBrackets : public CExp {
 public:
-	char c;
-	CExpBinary(CExp* x1, CExp* x2)
-	{
+	CExpInSquareBrackets( CExp* x1, CExp* x2 ) {
 		c1 = x1;
 		c2 = x2;
 	}
 
-	virtual void accept(visitor* v){
-		v->visit(this);
+	virtual void accept( visitor* v ) {
+		v->visit( this );
+	};
+};
+
+
+class CExpPointLENGTH : public CExp {
+public:
+	CExpPointLENGTH( CExp* x1, CExp* x2 ) {
+		c1 = x1;
+		c2 = x2;
+	}
+
+	virtual void accept( visitor* v ) {
+		v->visit( this );
+	};
+};
+
+
+class CExpPointID : public CExp {
+public:
+	CExpPointID( CExp* x1, CExp* x2, CExp* x3 ) {
+		c1 = x1;
+		c2 = x2;
+		c3 = x3
+	}
+
+	virtual void accept( visitor* v ) {
+		v->visit( this );
+	};
+};
+
+
+class CExpINTEGER_LITERAL : public CExp {
+public:
+	CExpINTEGER_LITERAL( CExp* x1 ) {
+		c1 = x1;
+	}
+
+	virtual void accept( visitor* v ) {
+		v->visit( this );
+	};
+};
+
+
+class CExpCExpSingleOp : public CExp {
+public:
+	CExpCExpSingleOp( CExp* x1 ) {
+		c1 = x1;
+	}
+
+	virtual void accept( visitor* v ) {
+		v->visit( this );
+	};
+};
+
+
+class CExpID : public CExp {
+public:
+	CExpID( CExp* x1 ) {
+		c1 = x1;
+	}
+
+	virtual void accept( visitor* v ) {
+		v->visit( this );
+	};
+};
+
+
+class CExpTHIS : public CExp {
+public:
+	CExpTHIS( CExp* x1 ) {
+		c1 = x1;
+	}
+
+	virtual void accept( visitor* v ) {
+		v->visit( this );
+	};
+};
+
+
+class CExpNEWINT : public CExp {
+public:
+	CExpNEWINT( CExp* x1, CExp* x2, CExp* x3 ) {
+		c1 = x1;
+		c2 = x2;
+		c3 = x3;
+	}
+
+	virtual void accept( visitor* v ) {
+		v->visit( this );
+	};
+};
+
+
+class CExpNEWID : public CExp {
+public:
+	CExpNEWID( CExp* x1, CExp* x2 ) {
+		c1 = x1;
+		c2 = x2;
+	}
+
+	virtual void accept( visitor* v ) {
+		v->visit( this );
+	};
+};
+
+
+class CExpExclamationMark : public CExp {
+public:
+	CExpExclamationMark( CExp* x1 ) {
+		c1 = x1;
+	}
+
+	virtual void accept( visitor* v ) {
+		v->visit( this );
+	};
+};
+
+
+class CExpCircleBrackets : public CExp {
+public:
+	CExpCircleBrackets( CExp* x1 ) {
+		c1 = x1;
+	}
+
+	virtual void accept( visitor* v ) {
+		v->visit( this );
 	};
 };
 
