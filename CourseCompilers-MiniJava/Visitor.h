422f3f52c2a964cddba126c15e5f1da0636f9d78
Nikolai <nvvaulin@gmail.com>
2015-11-08 00:25:24 +0300

    ast done

--- CourseCompilers-MiniJava/Visitor.h
+++ CourseCompilers-MiniJava/Visitor.h
@@ -1,129 +1,91 @@
 #pragma once
-#include "CTypes.h"
+class CProgram;
+class CMainClass;
+class CClassDecl;
+class CClassDeclInheritance;
+class CVarDecls;
+class CMethodDecls;
+class CVarDecl;
+class CMethodDecl;
+class CStatements;
+class CFormalList;
+class CFormalRests;
+class CFormalRest;
+class CType;
+class CExpList;
+class CExpRests;
+class CExpRest;
 
 
-class visitor
+class CStatementIF;
+class CStatementBRACKETS;
+class CStatementSQUEREASIGNMENT;
+class CStatementASIGNMENT;
+class CStatementPRINTLN;
+class CStatementWHILE;
+
+class CExpBinary;
+class CExpInSquareBrackets;
+class CExpPointLENGTH;
+class CExpPointID;
+class CExpINTEGER_LITERAL;
+class CExpSingleOp;
+class CExpID;
+class CExpTHIS;
+class CExpNEWINT;
+class CExpNEWID;
+class CExpExclamationMark;
+class CExpCircleBrackets;
+class CExpUnaryMinus;
+class CClassDecls;
+
+class IVisitor
 {
 public:
-	void visit(CStatementIF* n)
-	{
-		cout << "if ( ";
-		n->c1->accept(this);
-		cout << ") ";
-		n->c2->accept(this);
-		cout << " else ";
-		n->c3->accept(this);
-	};
+	virtual void visit(CStatementIF* n) = 0;
+	virtual void visit(CStatementBRACKETS* n) = 0;
 
-	void visit(CStatementBRACKETS* n)
-	{
-		cout << "{ ";
-		n->c1->accept(this);
-		cout << " }";
-	};
+	virtual void visit(CStatementSQUEREASIGNMENT* n) = 0;
 
-	void visit(CStatementSQUEREASIGNMENT* n)
-	{
-		n->c1->accept(this);
-		cout << "[ ";
-		n->c2->accept(this);
-		cout << " ] = ";
-		n->c3->accept(this);
-		cout << ";";
-	};
+	virtual void visit(CStatementASIGNMENT* n) = 0;
 
-	void visit(CStatementASIGNMENT* n)
-	{
-		n->c1->accept(this);
-		cout << " = ";
-		n->c2->accept(this);
-		cout << ";";
-	};
+	virtual void visit(CStatementPRINTLN* n) = 0;
 
-	void visit(CStatementPRINTLN* n)
-	{
-		cout << "( ";
-		n->c1->accept(this);
-		cout << ")";
-	};
+	virtual void visit(CStatementWHILE* n) = 0;
 
-	void visit(CStatementWHILE* n)
-	{
-		cout << "while ( ";
-		n->c1->accept(this);
-		cout << "0 ";
-		n->c2->accept(this);
-	};
+	virtual void visit(CExpBinary* n) = 0;
 
+	virtual void visit(CExpInSquareBrackets *n) = 0;
 
-	template< class T>
-	void visit(TStar<T>* n)
-	{
-		cout << " ";
-		for (auto i in n->v)
-		{
-			i->accept(this);
-			cout << " ";
-		};
-	}
-	void visit(CExpBinary* n)
-	{
-		n->c1->accept(this);
-		cout << n->c;
-		n->c2->accept(this);
-	}
-	void visit( CExpInSquareBrackets *n ) {
-		n->c1->accept( this );
-		cout << '[';
-		n->c2->accept( this );
-		cout << ']';
-	}
-	void visit( CExpPointLENGTH *n ) {
-		n->c1->accept( this );
-		cout << '.';
-		n->c2->accept( this );
-	}
-	void visit( CExpPointID *n ) {
-		n->c1->accept( this );
-		cout << '.';
-		n->c2->accept( this );
-		cout << '(';
-		n->c3->accept( this );
-		cout << ')';
-	}
-	void visit( CExpINTEGER_LITERAL *n ) {
-		n->c1->accept( this );
-	}
-	void visit( CExpSingleOp *n ) {
-		n->c1->accept( this );
-	}
-	void visit( CExpID *n ) {
-		n->c1->accept( this );
-	}
-	void visit( CExpTHIS *n ) {
-		n->c1->accept( this );
-	}
-	void visit( CExpNEWINT *n ) {
-		n->c1->accept( this );
-		n->c2->accept( this );
-		cout << '[';
-		n->c3->accept( this );
-		cout << ']';
-	}
-	void visit( CExpNEWID *n ) {
-		n->c1->accept( this );
-		n->c2->accept( this );
-		cout << '(' <<  ')';
-	}
-	void visit( CExpExclamationMark *n ) {
-		cout << '!'
-	}
-	void visit( CExpCircleBrackets *n ) {
-		cout << '(';
-		n->c1->accept( this );
-		cout << ')';
-	}
+	virtual void visit(CExpPointLENGTH *n) = 0;
 
-};
+	virtual void visit(CExpPointID *n) = 0;
 
+	virtual void visit(CExpINTEGER_LITERAL *n) = 0;
 
+	virtual void visit(CExpSingleOp *n) = 0;
+	virtual void visit(CExpID *n) = 0;
+	virtual void visit(CExpTHIS *n) = 0;
+	virtual void visit(CExpNEWINT *n) = 0;
+	virtual void visit(CExpNEWID *n) = 0;
+	virtual void visit(CExpExclamationMark *n) = 0;
+	virtual void visit(CExpCircleBrackets *n) = 0;
+	virtual void visit( CProgram *n ) = 0;
+	virtual void visit( CMainClass *n ) = 0;
+	virtual void visit( CClassDecl *n ) = 0;
+	virtual void visit(CClassDecls *n) = 0;
+	virtual void visit( CClassDeclInheritance *n ) = 0;
+	virtual void visit( CVarDecls *n ) = 0;
+	virtual void visit( CMethodDecls *n ) = 0;
+	virtual void visit( CVarDecl *n ) = 0;
+	virtual void visit( CMethodDecl *n ) = 0;
+	virtual void visit( CStatements *n ) = 0;
+	virtual void visit( CFormalList *n ) = 0;
+	virtual void visit( CFormalRests *n ) = 0;
+	virtual void visit( CFormalRest *n ) = 0;
+	virtual void visit( CExpList *n ) = 0;
+	virtual void visit( CType *n ) = 0;
+	virtual void visit( CExpRest *n ) = 0;
+	virtual void visit( CExpRests *n ) = 0;
+	virtual void visit(CExpUnaryMinus *n) = 0;
+}; 
